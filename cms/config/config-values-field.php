c2154d6e071db3563d7a040ae4c3c0cbafb230b9
Roger Glenn <roger@mach1media.com>
2023-05-03 15:44:46 -0500

    coding page builder neo block templates

---
+++ cms/config/config-values-field.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Color Values for dropdown
+ */
+
+return [
+	'data' => [
+		
+		'CSS Color Classes' => [
+			'default'              => 'Default',
+			'primary'              => 'Primary (Navy)',
+			'secondary'            => 'Secondary (Gold)',
+			'secondary-light'      => 'Gold: Light',
+			'secondary-extralight' => 'Gold: Light',
+			'dark'                 => 'Dark',
+			'silver'               => 'Silver',
+			'white'                => 'White',
+			'black'                => 'Black',
+		],
+		
+		// SECTION BACKGROUNDS
+		'Section Backgrounds' => [
+			'bg-default'              => 'Default',
+			'bg-primary'              => 'Primary (Navy)',
+			'bg-secondary'            => 'Secondary (Gold)',
+			'bg-secondary-light'      => 'Gold: Light',
+			'bg-secondary-extralight' => 'Gold: Extralight',
+			'bg-primary-light'        => 'Navy: Light',
+			'bg-primary-extralight'   => 'Navy: Extralight',
+			'bg-dark'                 => 'Dark',
+			'bg-silver'               => 'Silver',
+			'bg-white'                => 'White',
+			'bg-black'                => 'Black',
+		],
+		
+		// TEXT COLORS
+		'Text Colors' => [
+			'text-default'     => 'Default',
+			'text-primary'     => 'Primary (Navy)',
+			'text-secondary'   => 'Secondary (Gold)',
+			'text-dark'        => 'Dark',
+			'text-silver'      => 'Silver',
+			'text-white'       => 'White',
+			'text-black'       => 'Black',
+		],
+		
+		// BUTTON STYLE
+		'Button Style' => [
+			'btn-default'              => 'Default',
+			'btn-primary'              => 'Solid Primary (Navy)',
+			'btn-secondary'            => 'Solid Secondary (Gold)',
+			'btn-dark'                 => 'Solid Dark',
+			'btn-silver'               => 'Solid Silver',
+			'btn-white'                => 'Solid White',
+			'btn-black'                => 'Solid Black',
+			'btn-outline-primary'      => 'Outline Primary (Navy)',
+			'btn-outline-secondary'    => 'Outline Secondary (Gold)',
+			'btn-outline-dark'         => 'Outline Dark',
+			'btn-outline-silver'       => 'Outline Silver',
+			'btn-outline-white'        => 'Outline White',
+			'btn-outline-black'        => 'Outline Black',
+		],
+		
+		// CTA TILE LABELS
+		'CTA Tile Labels' => [
+			'grants'               => 'Grants',
+			'philanthropy'         => 'Philanthropy',
+			'investments'          => 'Investments',
+			'area-representatives' => 'Area Representatives',
+			'leadersnip-ministry'  => 'Leadership Ministry',
+		],
+	]
+];
+
