8bd898b1f3b7f35dd2710c434dc6bd3ebd5c0eb3
Roger Glenn <roger@mach1media.com>
2023-07-05 19:00:24 -0500

    swapped image tile hover/default state

--- src/js/app.js
+++ src/js/app.js
@@ -14,24 +14,41 @@ import * as bootstrap from 'bootstrap';
 
 // Import Plugins
 import '../vendor/fitvids/fitvids.js';
+import '../vendor/hoverIntent/jquery.hoverIntent.js';
 
 
 $(function() {
 	//
 	// mega menu toggle hovers
 	//
-	$('.js-nav-item').mouseenter(function(){
-		// reset all other open megamenus
-		$('.js-nav-item').attr('aria-expanded','false');
-		$('.c-header__mega-menu.show').removeClass('show');
+	$('.js-nav-item').hoverIntent(function(){
+		$(this).trigger('click');
+		// // reset all other open megamenus
+		// $('.js-nav-item').attr('aria-expanded','false');
+		// $('.c-header__mega-menu.show').removeClass('show');
 		
-		// open the corresponding megamenu
-		var megaMenuId = $(this).attr('data-bs-target');
-		if (megaMenuId) {
-			$(megaMenuId).addClass('show');
-			$(this).attr('aria-expanded','true');
-		}
+		// // open the corresponding megamenu
+		// var megaMenuId = $(this).attr('data-bs-target');
+		// if (megaMenuId) {
+		// 	$(megaMenuId).addClass('show');
+		// 	$(this).attr('aria-expanded','true');
+		// }
+	}, function(){
+		// do nothing
 	});
+
+	$('.js-megamenu').hoverIntent({
+		over: function(){
+			// do nothing
+		},
+		out: function(){
+			// reset all other open megamenus
+			$('.js-nav-item').attr('aria-expanded','false');
+			$('.c-header__mega-menu.show').removeClass('show');
+		},
+		timeout: 0
+	});
+
 	$(document).click(function(event) { 
 		var $target = $(event.target);
 		if (!$target.closest('.c-header__mega-menu.show').length && $('.c-header__mega-menu.show').is(":visible")) {
